ff5873851b4fc0e95f03693d2f5980ce5d992627
DannaGlz <l20100203@nlaredo.tecnm.mx>
2022-11-09 21:26:05 -0600

    Modulo

---
+++ ModuloCommoJS/app.js
@@ -0,0 +1,9 @@
+const modulo = require("./modulo");
+const cowsay = require("cowsay");
+console.log(modulo.cuadrado(800));
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e:"oO",
+    T : "U "
+    }));
